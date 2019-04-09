b572e14fb8d3a9266b57893a3d50b88d2495059c
biospank <fabio.petrucci@gmail.com>
2019-04-09 17:33:05 +0200

    Register a mdns service every 20 seconds

--- mdnservice.lua
+++ mdnservice.lua
@@ -1,7 +1,13 @@
-mdns.register(
-  "lampwireless", {
-    description="Light notifications",
-    service="lampwireless",
-    port=80
-  }
-)
+local mdnsTick = tmr.create()
+
+mdnsTick:alarm(20000, tmr.ALARM_AUTO, function()
+  print("Registering mdn service...")
+
+  mdns.register(
+    "lampwireless", {
+      description="Light notifications",
+      service="lampwireless",
+      port=80
+    }
+  )
+end)
